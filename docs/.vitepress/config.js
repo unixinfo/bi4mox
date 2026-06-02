2e106dc5563eea8cd7b402511ffd8b1bbed7fd7b
unixinfo <xiangxuliu@gmail.com>
2026-06-03 07:19:39 +0800

    update
    修改主页细节，修改线下活动页面并增加内容为链接指向微信公众号。

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -17,16 +17,16 @@ export default {
     
     sidebar: [
       {
-        text: "📡 业余无线电资料大全",
+        text: "📡 业余无线电资料",
         items: [
-          { text: '📜 法律法规', link: '/guide/01-法律法规' },
+          { text: '📜 法律法规相关', link: '/guide/01-法律法规' },
           { text: '📝 业余无线电考试', link: '/guide/02-业余无线电考试' },
-          { text: '📡 设台与许可', link: '/guide/03-设台与许可' },
-          { text: '🔧 设备介绍', link: '/guide/04-设备介绍' },
-          { text: '📶 天馈系统', link: '/guide/05-天馈系统' },
+          { text: '📡 设台与许可相关', link: '/guide/03-设台与许可' },
+          { text: '🔧 设备选择经验', link: '/guide/04-设备介绍' },
+          { text: '📶 天馈系统经验', link: '/guide/05-天馈系统' },
           { text: '📋 设台申请与变更', link: '/guide/06-首次设台及变更申请' },
-          { text: '🎉 业余活动', link: '/guide/07-业余活动' },
-          { text: '📞 中继台点名通联', link: '/guide/08-中继台点名通联记录' },
+          { text: '🎉 线下活动记录', link: '/guide/07-业余活动' },
+          { text: '📞 中继台点名通联记录', link: '/guide/08-中继台点名通联记录' },
           { text: '📊 中继台参数', link: '/guide/09-中继台参数' },
           { text: '❤️ 中继台捐赠', link: '/guide/10-中继台捐赠' }
         ]
@@ -34,7 +34,7 @@ export default {
     ],
     
     footer: {
-      copyright: "Copyright © 2025 业余无线电资料站"
+      copyright: "Copyright © 2026 业余无线电资料站"
     },
     
     // 最后更新时间
